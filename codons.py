95af635ad0baf3d6be3fc0166492f405998495e3
beraaht <bertam@ucdavis.edu>
2020-04-11 21:58:59 -0700

    changes

--- codons.py
+++ codons.py
@@ -11,9 +11,17 @@ print(dna[3:6])
 print(dna[6:9])
 print(dna[9:12])
 """
-#For loop prints out all codons in increments of 3 
-for i in range(0, 27, 3): # 27 total characters, in increments of 3
-	print(dna[i:i+3])
+# For loop prints out all codons in increments of 3 
+#for i in range(0, 27, 3): # 27 total characters, in increments of 3
+#	print(dna[i:i+3])
+
+# Nested loop
+for f in range(3):
+	print('frame', f)
+	for i in range(0, len(dna) -2, 3):
+		codon = dna[i:i+3]
+		print(codon)
+
 """
 ATA
 GCG
